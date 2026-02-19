62ab2c1322398a4176e6deb08965c809d7bebc36
frostyesquivel-dev <frostyesquivel@gmail.com>
2026-02-19 10:17:25 -0600

    Create camera test.py

---
+++ camera test.py
@@ -0,0 +1,9 @@
+``` source venv/bin/activate 
+``` cd /home/jonahesq/ultralytics
+
+``` python stable_dual.py
+
+``` python lowcon_test.py
+5%
+``` python noperc_test.py
+percentage removed from bounding box
